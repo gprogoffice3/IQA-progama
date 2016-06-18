c5752666d3bcb3c6eef843ccc9406830baed82e9
gprogoffice3 <gprogoffice@gmail.com>
2016-06-17 23:38:21 -0300

    Update baseiqa versaofinal.c

--- baseiqa versaofinal.c
+++ baseiqa versaofinal.c
@@ -6,17 +6,18 @@
 int main (void)  {
   double q1, q2, q3, q4, q5, q6, q7, q8, q9, OXIGENIO, cf, ph, dbo, ft, nt, tu, deltaT, st, TOTAL;
 
-       // 
-       printf("coliformes fecais :");
-       scanf("%lf", &cf);
+     // CALCULANDO PARÂMETROS DE QUALIDADE DA ÁGUA
+     printf("coliformes fecais :");
+     scanf("%lf", &cf);
           if (cf > 100000) {
        q1 = 3.0;
 
       }else{
         q1 = 98.03 + (( -36.45 ) * log(cf)) + 3.138 * pow(log10(cf),2) + 0.06776 * pow (log10(cf),3)); 
         }
-       printf("PH :");
-       scanf("%lf", &ph);
+      //POTENCIAL HIDROGENIÔNICO
+      printf("PH :");
+      scanf("%lf", &ph);
           if ( ph > 12) {
          q2 = 3.0;
           }else if (ph > 2){ 
@@ -24,6 +25,7 @@ int main (void)  {
       }else{
          q2 = (0.05421 * pow(ph, 1.23 * ph + (  -0,09873 ) * pow(ph,2)) + 5,213);
           }
+        //DEMANDA BIOQUIMICA DE OXIGÊNIO  
        printf("DBO :");
        scanf("%lf", &dbo);
         if (dbo > 30){
@@ -32,6 +34,7 @@ int main (void)  {
       }else{ 
            q3 = 102.6 * exp(-0.1101 * dbo);
         }
+        //FÓSFORO TOTAL
           printf("FT :");
           scanf("%lf", &ft);
             if (ft > 10) {
@@ -40,14 +43,16 @@ int main (void)  {
         }else{
             q5 = 213.7 * exp(-1.680 ) * pow(ft, 0.3325));
           }
-          printf("NT :");
-          scanf("%lf", &nt);
+         //NITROGÊNIO TOTAL
+        printf("NT :");
+        scanf("%lf", &nt);
             if (nt > 100){
             q4 = 1.0;
 
         }else{
             q4 = 98.96 * pow(nt, (-0,2232 + (-0.00647) * nt );
             }
+          //TURBIDEZ
           printf("TU :");
           scanf("%lf", &tu);
             if (tu > 100){
@@ -71,6 +76,7 @@ int main (void)  {
         }else{
               q8 =  (80.26 * exp( ((-0.00107) * st) + (0,03009 * sqrt(st)) ) + (-0,1185) * st );
             }
+            //OXIGÊNIO
             printf("oxigenio :");
             scanf("%lf", &OXIGENIO);
               if (OXIGENIO == 140){
@@ -79,6 +85,7 @@ int main (void)  {
                q9 = 100 * 8 exp(pow(OXIGENIO + (-106), 2) / -3745);
 
         }
+        //RESULTADO
         TOTAL = pow(q1, 0.15) * pow(q2, 0.12) * pow(q3, 0.10) * pow(q5, 0.10) * pow(q4, 0.10) * pow(q7, 0.08) * pow(q6, 0.1) * pow(q8, 0.08) * pow (q9, 0.17);
               if (TOTAL > 79 && TOTAL <= 100 ){
             printf("%lf - Qualidade Ótima\n ", TOTAL); 
